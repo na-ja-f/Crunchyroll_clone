32f5a6a878b832d72d2d0ddd30b3af36fbde840e
na-ja-f <najafnaju1983@gmail.com>
2024-08-13 15:09:38 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+
+function Navbar() {
+  return (
+    <div className='bg-[#23252b] h-14 flex justify-between px-24 items-center'>
+      <div className='flex'>
+        <div className=''>
+            <img src={logo} alt="logo" className='h-40'/>
+        </div>
+        <div className=''>
+            
+        </div>
+        <div className=''>
+            
+        </div>
+        <div className=''>
+            
+        </div>
+      </div>
+      <div>
+        <h1>helllo</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default HomePage
